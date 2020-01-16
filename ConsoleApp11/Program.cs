6f7f0b3a32bb425fe664a5f8f47de01a87eb20dc
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-16 16:58:26 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z;
+            Console.WriteLine("Tell me your mass");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Tell me your height");
+            y = Convert.ToDouble(Console.ReadLine());
+            z = (x / (y * y));
+            Console.Write("Your BMI is: ");
+            Console.Write(z);
+            Console.ReadKey();
+        }
+    }
+}
